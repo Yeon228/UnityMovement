5ebe205494cc6c9d6c48ec15d96f66bf181f19a1
yeon228 <cumer88@naver.com>
2022-07-01 00:44:56 +0900

    make project

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public Camera cam;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Move();
+       Rotation();
+    }
+
+    void Move()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 100, Time.deltaTime * 2);
+            transform.position += transform.forward * vertical * 3f * Time.deltaTime;
+            transform.position += transform.right * horizontal * 3f * Time.deltaTime;
+        }
+        else
+        {
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 70, Time.deltaTime * 2);
+            transform.position += transform.forward * vertical  * Time.deltaTime;
+            transform.position += transform.right * horizontal  * Time.deltaTime;
+        }
+    }
+
+    void Rotation()
+    {
+        transform.rotation = transform.rotation * Quaternion.Euler(0, Input.GetAxis("Mouse X") * 10f, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator ctr;
+    private Rigidbody rig;
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        ctr.SetBool("space", false);
+        ctr.SetBool("shift", false);
+        ctr.SetBool("isMove", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ctr.SetFloat("yAxis" , transform.position.y);
+        ctr.SetFloat("vertical", Input.GetAxis("Vertical"));
+        ctr.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) 
+        {
+            ctr.SetBool("isMove", true);
+        }
+        else
+        {
+            ctr.SetBool("isMove", false);
+        }
+        ctr.SetBool("shift" , Input.GetKey(KeyCode.LeftShift));
+        if (Input.GetKeyDown(KeyCode.Space) && !ctr.GetCurrentAnimatorStateInfo(0).IsTag("jump"))
+        {
+            rig.AddForce(transform.up * 3f, ForceMode.Impulse);
+            ctr.SetBool("space" , true);
+            
+        }
+        else
+        {
+            ctr.SetBool("space" , false);
+        }
+    }
+}
